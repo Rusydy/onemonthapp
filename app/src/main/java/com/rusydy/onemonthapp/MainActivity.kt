4f5797b9f93ec1da9a8f09f798dd71e0d6111355
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-09-30 01:42:23 +0700

    Add VCS configuration and update RecipeCard styling

--- app/src/main/java/com/rusydy/onemonthapp/MainActivity.kt
+++ app/src/main/java/com/rusydy/onemonthapp/MainActivity.kt
@@ -3,19 +3,15 @@ package com.rusydy.onemonthapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -25,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rusydy.onemonthapp.data.Recipe
@@ -76,13 +71,22 @@ fun RecipeCard(recipe: Recipe) {
             .fillMaxWidth()
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = "Day ${recipe.day}: ${recipe.title}", style = MaterialTheme.typography.titleSmall)
+            Text(
+                text = "Day ${recipe.day}: ${recipe.title}",
+                style = MaterialTheme.typography.titleSmall
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = recipe.description, style = MaterialTheme.typography.bodyMedium)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Ingredients: ${recipe.ingredients.joinToString(", ")}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Ingredients: ${recipe.ingredients.joinToString(", ")}",
+                style = MaterialTheme.typography.bodyMedium
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Instructions: ${recipe.instructions}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Instructions: ${recipe.instructions}",
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
